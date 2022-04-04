0bf8342782a326ecd14483b2026f07a26b98486b
Okan Ulu <okanulu048@gmail.com>
2022-04-04 12:43:16 +0300

    prelab2

--- LoginPage.cs
+++ LoginPage.cs
@@ -3,11 +3,13 @@ namespace OOP_LAB
     public partial class LoginPage : Form
     {
         public static List<User> UserList = new List<User>();
-        public static string UserFilePath = @"data/data.csv";
+        public static string UserFilePath = "C:/Users/Okan/source/repos/OOP_LAB/OOP_LAB/data.csv";
+        string Lastuser = Settings.Default.Lastuser;
         public LoginPage()
 
         {
             InitializeComponent();
+            txtUsername.Text = Lastuser;
             Util.LoadCsv(UserList, UserFilePath);
         }
 
@@ -15,15 +17,18 @@ namespace OOP_LAB
         {
             string Username = txtUsername.Text;
             string Password = txtPassword.Text;
-            string Lastuser = 
-
+            
+            
             for (int i = 0; i < UserList.Count; i++)
             {
                 User user = UserList[i];
                 if (user.UserName == Username && user.Password == Password)
                 {
+                    Settings.Default.Lastuser = Username;
+                    Settings.Default.Save();
+
                     LoginUser.getInstance().User = user;
-                    Lastuser = 
+                    
                     MainPage main = new MainPage();
                     this.Hide();
                     main.Show();
@@ -33,12 +38,37 @@ namespace OOP_LAB
                     LblLoginMessage.Text = "Warning!! USERNAME/PASSWORD is incorrect";
                     LblLoginMessage.ForeColor = Color.Red;
                     LblLoginMessage.Visible = true;
+                    
+                }
+                if(Password != Username)
+                {
+                    Settings.Default.Lastuser = null;
+                    Settings.Default.Save();
                 }
             }
 
         
 
 
+        }
+
+        private void txtUsername_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = !char.IsLetter(e.KeyChar) && !char.IsControl(e.KeyChar)
+
+            && !char.IsSeparator(e.KeyChar);
+        }
+
+        private void Goster_CheckedChanged(object sender, EventArgs e)
+        {
+            if (Goster.Checked == true )
+            {
+                txtPassword.PasswordChar = '\0';
+            }
+            else
+            {
+                txtPassword.PasswordChar = '*';
+            }
         }
 
     }
