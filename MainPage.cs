50d67e0f15fff6c733904ee67a75b6abeff95efb
Okan Ulu <okanulu048@gmail.com>
2022-04-11 00:12:26 +0300

    added

---
+++ MainPage.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OOP_LAB
+{
+    public partial class MainPage : Form
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)//settings ekranına geçişi sağlar.
+        {
+            SettingsPage settings = new SettingsPage();
+            settings.ShowDialog();
+           // this.Hide();
+
+        }
+
+        private void MainPage_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_profilescreen_Click(object sender, EventArgs e)
+        {
+            ProfileScreen profileScreen = new ProfileScreen();
+            profileScreen.ShowDialog();
+        }
+    }
+}
---
+++ ProfileScreen.Designer.cs
@@ -0,0 +1,307 @@
+﻿namespace OOP_LAB
+{
+    partial class ProfileScreen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_user = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbl_1 = new System.Windows.Forms.Label();
+            this.txt_email_profile = new System.Windows.Forms.TextBox();
+            this.txt_country_profile = new System.Windows.Forms.TextBox();
+            this.txt_city_profile = new System.Windows.Forms.TextBox();
+            this.txt_address_profile = new System.Windows.Forms.TextBox();
+            this.txt_phonenumber_profile = new System.Windows.Forms.TextBox();
+            this.txt_namesurname_profile = new System.Windows.Forms.TextBox();
+            this.txt_password_profile = new System.Windows.Forms.TextBox();
+            this.txt_username_profile = new System.Windows.Forms.TextBox();
+            this.btn_saveprofile = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.currentpassword = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbl_user
+            // 
+            this.lbl_user.AutoSize = true;
+            this.lbl_user.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lbl_user.Location = new System.Drawing.Point(579, 19);
+            this.lbl_user.Name = "lbl_user";
+            this.lbl_user.Size = new System.Drawing.Size(51, 20);
+            this.lbl_user.TabIndex = 0;
+            this.lbl_user.Text = "label1";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label7.Location = new System.Drawing.Point(515, 312);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(57, 20);
+            this.label7.TabIndex = 32;
+            this.label7.Text = "E-mail ";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label6.Location = new System.Drawing.Point(515, 279);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(66, 20);
+            this.label6.TabIndex = 31;
+            this.label6.Text = "Country";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(515, 246);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(36, 20);
+            this.label5.TabIndex = 30;
+            this.label5.Text = "City";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(515, 213);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(66, 20);
+            this.label4.TabIndex = 29;
+            this.label4.Text = "Address";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(515, 180);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(115, 20);
+            this.label3.TabIndex = 28;
+            this.label3.Text = "Phone Number";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(515, 147);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(119, 20);
+            this.label2.TabIndex = 27;
+            this.label2.Text = "Name-Surname";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(515, 114);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 20);
+            this.label1.TabIndex = 26;
+            this.label1.Text = "Password";
+            // 
+            // lbl_1
+            // 
+            this.lbl_1.AutoSize = true;
+            this.lbl_1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lbl_1.Location = new System.Drawing.Point(515, 81);
+            this.lbl_1.Name = "lbl_1";
+            this.lbl_1.Size = new System.Drawing.Size(83, 20);
+            this.lbl_1.TabIndex = 25;
+            this.lbl_1.Text = "UserName";
+            // 
+            // txt_email_profile
+            // 
+            this.txt_email_profile.Location = new System.Drawing.Point(648, 305);
+            this.txt_email_profile.Name = "txt_email_profile";
+            this.txt_email_profile.Size = new System.Drawing.Size(125, 27);
+            this.txt_email_profile.TabIndex = 24;
+            // 
+            // txt_country_profile
+            // 
+            this.txt_country_profile.Location = new System.Drawing.Point(648, 272);
+            this.txt_country_profile.Name = "txt_country_profile";
+            this.txt_country_profile.Size = new System.Drawing.Size(125, 27);
+            this.txt_country_profile.TabIndex = 23;
+            // 
+            // txt_city_profile
+            // 
+            this.txt_city_profile.Location = new System.Drawing.Point(648, 239);
+            this.txt_city_profile.Name = "txt_city_profile";
+            this.txt_city_profile.Size = new System.Drawing.Size(125, 27);
+            this.txt_city_profile.TabIndex = 22;
+            // 
+            // txt_address_profile
+            // 
+            this.txt_address_profile.Location = new System.Drawing.Point(648, 206);
+            this.txt_address_profile.Name = "txt_address_profile";
+            this.txt_address_profile.Size = new System.Drawing.Size(125, 27);
+            this.txt_address_profile.TabIndex = 21;
+            // 
+            // txt_phonenumber_profile
+            // 
+            this.txt_phonenumber_profile.Location = new System.Drawing.Point(648, 173);
+            this.txt_phonenumber_profile.Name = "txt_phonenumber_profile";
+            this.txt_phonenumber_profile.Size = new System.Drawing.Size(125, 27);
+            this.txt_phonenumber_profile.TabIndex = 20;
+            // 
+            // txt_namesurname_profile
+            // 
+            this.txt_namesurname_profile.Location = new System.Drawing.Point(648, 140);
+            this.txt_namesurname_profile.Name = "txt_namesurname_profile";
+            this.txt_namesurname_profile.Size = new System.Drawing.Size(125, 27);
+            this.txt_namesurname_profile.TabIndex = 19;
+            // 
+            // txt_password_profile
+            // 
+            this.txt_password_profile.Location = new System.Drawing.Point(648, 107);
+            this.txt_password_profile.Name = "txt_password_profile";
+            this.txt_password_profile.Size = new System.Drawing.Size(125, 27);
+            this.txt_password_profile.TabIndex = 18;
+            // 
+            // txt_username_profile
+            // 
+            this.txt_username_profile.Location = new System.Drawing.Point(648, 74);
+            this.txt_username_profile.Name = "txt_username_profile";
+            this.txt_username_profile.Size = new System.Drawing.Size(125, 27);
+            this.txt_username_profile.TabIndex = 17;
+            // 
+            // btn_saveprofile
+            // 
+            this.btn_saveprofile.Location = new System.Drawing.Point(648, 397);
+            this.btn_saveprofile.Name = "btn_saveprofile";
+            this.btn_saveprofile.Size = new System.Drawing.Size(125, 29);
+            this.btn_saveprofile.TabIndex = 33;
+            this.btn_saveprofile.Text = "Save Profile";
+            this.btn_saveprofile.UseVisualStyleBackColor = true;
+            this.btn_saveprofile.Click += new System.EventHandler(this.btn_saveprofile_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label8.ForeColor = System.Drawing.Color.Red;
+            this.label8.Location = new System.Drawing.Point(527, 43);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(273, 20);
+            this.label8.TabIndex = 34;
+            this.label8.Text = "You can only change own information";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label9.Location = new System.Drawing.Point(515, 369);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(133, 20);
+            this.label9.TabIndex = 36;
+            this.label9.Text = "Current Password";
+            // 
+            // currentpassword
+            // 
+            this.currentpassword.Location = new System.Drawing.Point(648, 362);
+            this.currentpassword.Name = "currentpassword";
+            this.currentpassword.Size = new System.Drawing.Size(125, 27);
+            this.currentpassword.TabIndex = 35;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(341, 335);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(447, 20);
+            this.label10.TabIndex = 37;
+            this.label10.Text = "Bilgileri kaydetmek için giriş yaparken kullandığınız şifrenizi giriniz";
+            // 
+            // ProfileScreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.currentpassword);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.btn_saveprofile);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lbl_1);
+            this.Controls.Add(this.txt_email_profile);
+            this.Controls.Add(this.txt_country_profile);
+            this.Controls.Add(this.txt_city_profile);
+            this.Controls.Add(this.txt_address_profile);
+            this.Controls.Add(this.txt_phonenumber_profile);
+            this.Controls.Add(this.txt_namesurname_profile);
+            this.Controls.Add(this.txt_password_profile);
+            this.Controls.Add(this.txt_username_profile);
+            this.Controls.Add(this.lbl_user);
+            this.Name = "ProfileScreen";
+            this.Text = "ProfileScreen";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label lbl_user;
+        private Label label7;
+        private Label label6;
+        private Label label5;
+        private Label label4;
+        private Label label3;
+        private Label label2;
+        private Label label1;
+        private Label lbl_1;
+        private TextBox txt_email_profile;
+        private TextBox txt_country_profile;
+        private TextBox txt_city_profile;
+        private TextBox txt_address_profile;
+        private TextBox txt_phonenumber_profile;
+        private TextBox txt_namesurname_profile;
+        private TextBox txt_password_profile;
+        private TextBox txt_username_profile;
+        private Button btn_saveprofile;
+        private Label label8;
+        private Label label9;
+        private TextBox currentpassword;
+        private Label label10;
+    }
+}
